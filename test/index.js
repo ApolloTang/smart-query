9e80a467612d35a8c2c6e4ea06cf6ca84fea887c
Apollo Tang <tang.apollo@gmail.com>
2016-07-31 13:26:53 -0400

    add annotate for test description

--- test/index.js
+++ test/index.js
@@ -21,30 +21,54 @@ var  note = '';
 
 var stringToSearch_standard = '#123 the #qu#ick fox jump over the lazy $10.00 dog #1945';
 
-note = '[1]';
+note = '[1] single token of id';
 stringToSearch = stringToSearch_standard;
 searchQuery = '#123';
 assert.equal(search(searchQuery, stringToSearch, note), true);
 
-note = '[2]';
+note = '[2] words not in order, contain id ';
 stringToSearch = stringToSearch_standard;
 searchQuery = 'the #123';
 assert.equal(search(searchQuery, stringToSearch, note), true);
 
-note = '[3]';
+note = '[3] quote as a token';
 stringToSearch = stringToSearch_standard;
 searchQuery = 'the "jump over the" #123';
 assert.equal(search(searchQuery, stringToSearch, note), true);
 
-note = '[4] partial';
+note = '[4] partial word';
 stringToSearch = stringToSearch_standard;
 searchQuery = 'og';
 assert.equal(search(searchQuery, stringToSearch, note), true);
-
 
 note = '[5] partial at the end';
 stringToSearch = stringToSearch_standard;
 searchQuery = '45';
 assert.equal(search(searchQuery, stringToSearch, note), true);
 
+
+note = '[6] partial at the end with \s';
+stringToSearch = stringToSearch_standard;
+searchQuery = '45';
+assert.equal(search(searchQuery, stringToSearch, note), true);
+
+note = '[7] [fail case] partial at the end with \s';
+stringToSearch = stringToSearch_standard;
+searchQuery = '457';
+assert.equal(search(searchQuery, stringToSearch, note), false);
+
+note = '[8] contain regex char';
+stringToSearch = stringToSearch_standard;
+searchQuery = 'the $10.00 fox';
+assert.equal(search(searchQuery, stringToSearch, note), true);
+
+note = '[9] contain regex char';
+stringToSearch = stringToSearch_standard;
+searchQuery = 'the $';
+assert.equal(search(searchQuery, stringToSearch, note), true);
+
+note = '[10] [fail case] contain regex char, un match string';
+stringToSearch = stringToSearch_standard;
+searchQuery = 'the $20.00';
+assert.equal(search(searchQuery, stringToSearch, note), false);
 console.log('=========================================== test done');
