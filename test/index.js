d8dae088af2223cec8a117c0c5a5793e85835268
Apollo Tang <tang.apollo@gmail.com>
2016-07-31 12:43:43 -0400

    'able to do test now'

--- test/index.js
+++ test/index.js
@@ -1,5 +1,32 @@
 var assert = require('assert');
-var main = require('../');
-main('hello world bye world');
+var searchStringTokensRegExGenerator = require('../');
 
-console.log('test done');
+function search(searchQuery, text, note) {
+    var found = searchStringTokensRegExGenerator(searchQuery).test(text);
+    console.log('------------------------- test result start')
+    var note = note || '';
+    console.log('note:', note);
+    console.log('string to search: ', text);
+    console.log('search query: ', searchQuery);
+    console.log('found: ', found );
+    console.log('------------------------- test result end');
+    console.log('');
+    return found;
+}
+
+var  stringToSearch = '';
+var  searchQuery = '';
+var  note = '';
+
+
+note = '[1]';
+stringToSearch = 'Hello world';
+searchQuery = 'Hello';
+assert.equal(search(searchQuery, stringToSearch, note), true);
+
+note = '[2]';
+stringToSearch = 'Hello world';
+searchQuery = 'world hello';
+assert.equal(search(searchQuery, stringToSearch, note), true);
+
+console.log('=========================================== test done');
